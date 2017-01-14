50398b874527b72ec067c785d7b6a1a5d8af4557
Jimmy Huang <Jimmy.Huang@creditkarma.com>
2017-01-14 00:33:17 -0800

    reordered the vmtools

--- provisioning-scripts/clean-up.sh
+++ provisioning-scripts/clean-up.sh
@@ -1,46 +1,34 @@
 #!/usr/bin/env bash
 
+echo "==> Clear out machine id"
+rm -f /etc/machine-id
+touch /etc/machine-id
+
 echo "==> Cleaning up temporary network addresses"
 # Make sure udev doesn't block our network
+if grep -q -i "release 6" /etc/redhat-release ; then
+    rm -f /etc/udev/rules.d/70-persistent-net.rules
+    mkdir /etc/udev/rules.d/70-persistent-net.rules
+    rm /lib/udev/rules.d/75-persistent-net-generator.rules
+fi
 rm -rf /dev/.udev/
 if [ -f /etc/sysconfig/network-scripts/ifcfg-eth0 ] ; then
     sed -i "/^HWADDR/d" /etc/sysconfig/network-scripts/ifcfg-eth0
     sed -i "/^UUID/d" /etc/sysconfig/network-scripts/ifcfg-eth0
 fi
 
-# new-style network device naming for centos7
-
-# radio off & remove all interface configration
-nmcli radio all off
-/bin/systemctl stop NetworkManager.service
-for ifcfg in `ls /etc/sysconfig/network-scripts/ifcfg-* |grep -v ifcfg-lo` ; do
-rm -f $ifcfg
-done
-rm -rf /var/lib/NetworkManager/*
-
-echo "==> Setup /etc/rc.d/rc.local for CentOS7"
-cat <<_EOF_ | cat >> /etc/rc.d/rc.local
-#BOXCUTTER-BEGIN
-LANG=C
-# delete all connection
-for con in \`nmcli -t -f uuid con\`; do
-  if [ "\$con" != "" ]; then
-    nmcli con del \$con
-  fi
-done
-# add gateway interface connection.
-gwdev=\`nmcli dev | grep ethernet | egrep -v 'unmanaged' | head -n 1 | awk '{print \$1}'\`
-if [ "\$gwdev" != "" ]; then
-  nmcli c add type eth ifname \$gwdev con-name \$gwdev
-fi
-sed -i -e "/^#BOXCUTTER-BEGIN/,/^#BOXCUTTER-END/{s/^/# /}" /etc/rc.d/rc.local
-chmod -x /etc/rc.d/rc.local
-#BOXCUTTER-END
-_EOF_
-chmod +x /etc/rc.d/rc.local
+# Fix for https://github.com/CentOS/sig-cloud-instance-build/issues/38
+cat > /etc/sysconfig/network-scripts/ifcfg-eth0 << EOF
+DEVICE="eth0"
+BOOTPROTO="dhcp"
+ONBOOT="yes"
+TYPE="Ethernet"
+PERSISTENT_DHCLIENT="yes"
+EOF
 
 DISK_USAGE_BEFORE_CLEANUP=$(df -h)
 
+# Other locales will be removed from the VM
 echo "==> Remove unused man page locales"
 pushd /usr/share/man
 if [ $(ls | wc -w) -gt 16 ]; then
