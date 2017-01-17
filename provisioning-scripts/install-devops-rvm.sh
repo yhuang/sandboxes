2f6552de585e738d47a93501fea81d45384eff86
Jimmy Y. Huang <jimmy.huang@duragility.com>
2017-01-16 19:36:21 -0800

    simplified devops box and updated terraform version

--- provisioning-scripts/install-devops-rvm.sh
+++ provisioning-scripts/install-devops-rvm.sh
@@ -7,11 +7,3 @@ curl -sSL https://get.rvm.io | bash -s stable --ruby
 source $HOME/.rvm/scripts/rvm
 
 gem install bundler
-
-echo "==> Install additional Ruby versions"
-for VERSION in 2.2.2
-do
-	rvm install $VERSION
-	rvm use $VERSION
-	gem install bundler
-done
