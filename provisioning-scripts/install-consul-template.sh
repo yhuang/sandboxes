f029bb6e65d978e8edf1620b4e9839ccd0d81787
Jimmy Y. Huang <jimmy.huang@duragility.com>
2017-01-22 22:51:18 -0800

    upgraded consul-template version

--- provisioning-scripts/install-consul-template.sh
+++ provisioning-scripts/install-consul-template.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 APPLICATION=consul-template
-VERSION=0.16.0
+VERSION=0.18.0
 ARCHIVE=${APPLICATION}_${VERSION}_linux_amd64.zip
 
 wget https://releases.hashicorp.com/$APPLICATION/$VERSION/$ARCHIVE
