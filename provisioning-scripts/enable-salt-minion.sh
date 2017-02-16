ffd329ed7c35f2b4a4fb11b4f9e523d629e9217f
Jimmy Huang <Jimmy.Huang@creditkarma.com>
2017-02-15 17:39:12 -0800

    not going to learn salt

--- provisioning-scripts/enable-salt-minion.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-systemctl enable salt-minion
-systemctl start salt-minion
--- provisioning-scripts/install-bootstrap-salt.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-wget -O /usr/local/bin/bootstrap_salt.sh https://bootstrap.saltstack.com
-chmod a+x /usr/local/bin/bootstrap_salt.sh
