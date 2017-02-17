863ce8ada982134e7382f799dbb15c02f5b716b3
Jimmy Y. Huang <jimmy.huang@duragility.com>
2017-02-17 07:05:00 -0800

    rvm/ruby not fully working well, but we are half way there

--- provisioning-scripts/install-devops-rvm.sh
+++ provisioning-scripts/install-devops-rvm.sh
@@ -2,8 +2,21 @@
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 
-curl -sSL https://get.rvm.io | bash -s stable --ruby
+curl -sSL https://get.rvm.io | bash -s stable
 
-source $HOME/.rvm/scripts/rvm
+RUBY_VERSION=2.4.0
+RVM_SCRIPT=$HOME/.rvm/scripts/rvm
 
-gem install bundler
+source $RVM_SCRIPT
+echo "\nsource $RVM_SCRIPT" >> $HOME/.bash_profile
+
+rvm install $RUBY_VERSION
+rvm use $RUBY_VERSION --default
+
+rvm gemset create devops
+rvm gemset use devops
+
+gem install \
+aws-sdk \
+bundler \
+inspec
