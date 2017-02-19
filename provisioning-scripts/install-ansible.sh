3be43dfb2433bd84d4d5764d387ad10688dd23b7
Jimmy Huang <Jimmy.Huang@creditkarma.com>
2017-02-18 23:28:56 -0800

    improved ansible and devops box builds

--- provisioning-scripts/install-ansible.sh
+++ provisioning-scripts/install-ansible.sh
@@ -7,11 +7,6 @@ mkdir -p $ANSIBLE_DIR/ssh && chmod 700 $ANSIBLE_DIR/ssh
 mv /tmp/ansible.cfg $ANSIBLE_DIR/ansible.cfg
 chown root:root $ANSIBLE_DIR/ansible.cfg
 
-VAGRANT_USER_HOME=/home/vagrant
-
-mv /tmp/vagrant $VAGRANT_USER_HOME/.ssh/vagrant
-chmod 600 $VAGRANT_USER_HOME/.ssh/vagrant
-
 yum install -y python-jinja2 python-pyasn1
 yum --disablerepo="*" --enablerepo="epel" install -y python-keyczar
 yum --disablerepo="*" --enablerepo="epel" install -y ansible
