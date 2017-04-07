58abdd0bc6bc768432ef292d40c87842d19b6e63
Jimmy Y. Huang <jimmy.huang@duragility.com>
2017-04-07 06:11:12 -0700

    upgraded hashicorp software

--- provisioning-scripts/install-consul-template.sh
+++ provisioning-scripts/install-consul-template.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 APPLICATION=consul-template
-VERSION=0.18.1
+VERSION=0.18.2
 ARCHIVE=${APPLICATION}_${VERSION}_linux_amd64.zip
 
 wget https://releases.hashicorp.com/$APPLICATION/$VERSION/$ARCHIVE
--- provisioning-scripts/install-packer.sh
+++ provisioning-scripts/install-packer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 APPLICATION=packer
-VERSION=0.12.3
+VERSION=1.0.0
 ARCHIVE=${APPLICATION}_${VERSION}_linux_amd64.zip
 
 wget https://releases.hashicorp.com/$APPLICATION/$VERSION/$ARCHIVE
