0a7ea25b7e116722ec105205bd6d7ded25fe997a
Jimmy Y. Huang <jimmy.huang@duragility.com>
2017-08-19 22:25:46 -0700

    upgraded HashiCorp software

--- provisioning-scripts/install-packer.sh
+++ provisioning-scripts/install-packer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 APPLICATION=packer
-VERSION=1.0.0
+VERSION=1.0.4
 ARCHIVE=${APPLICATION}_${VERSION}_linux_amd64.zip
 
 wget https://releases.hashicorp.com/$APPLICATION/$VERSION/$ARCHIVE
--- provisioning-scripts/install-vault.sh
+++ provisioning-scripts/install-vault.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 APPLICATION=vault
-VERSION=0.7.3
+VERSION=0.8.1
 ARCHIVE=${APPLICATION}_${VERSION}_linux_amd64.zip
 
 wget https://releases.hashicorp.com/$APPLICATION/$VERSION/$ARCHIVE
