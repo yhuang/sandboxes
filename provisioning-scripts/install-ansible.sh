d2b3bf1c48131a6dcb440a706174f5249b40df26
Jimmy Y. Huang <jimmy.huang@duragility.com>
2017-08-30 19:14:33 -0700

    ansible requires PyYAML

--- provisioning-scripts/install-ansible.sh
+++ provisioning-scripts/install-ansible.sh
@@ -7,6 +7,6 @@ mkdir -p $ANSIBLE_DIR/ssh && chmod 700 $ANSIBLE_DIR/ssh
 mv /tmp/ansible.cfg $ANSIBLE_DIR/ansible.cfg
 chown root:root $ANSIBLE_DIR/ansible.cfg
 
-yum install -y python-jinja2 python-pyasn1
+yum install -y PyYAML python-jinja2 python-pyasn1 PyYAML
 yum --disablerepo="*" --enablerepo="epel" install -y python-keyczar
 yum --disablerepo="*" --enablerepo="epel" install -y ansible
