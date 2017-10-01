19f5dcaae153f81876aa43e20fbfdb4083272cf1
Jimmy Y. Huang <jimmy.huang@duragility.com>
2017-10-01 01:07:41 -0700

    installing the latest stable version of ansible

--- provisioning-scripts/install-ansible.sh
+++ provisioning-scripts/install-ansible.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+VERSION=2.4
+
+yum install -y libffi-devel openssl-devel python-devel python-sphinx PyYAML
+
 ANSIBLE_DIR=/etc/ansible
 
 mkdir -p $ANSIBLE_DIR/ssh && chmod 700 $ANSIBLE_DIR/ssh
@@ -7,6 +11,12 @@ mkdir -p $ANSIBLE_DIR/ssh && chmod 700 $ANSIBLE_DIR/ssh
 mv /tmp/ansible.cfg $ANSIBLE_DIR/ansible.cfg
 chown root:root $ANSIBLE_DIR/ansible.cfg
 
-yum install -y PyYAML python-jinja2 python-pyasn1 PyYAML
-yum --disablerepo="*" --enablerepo="epel" install -y python-keyczar
-yum --disablerepo="*" --enablerepo="epel" install -y ansible
+git clone git://github.com/ansible/ansible.git
+cd ansible
+git checkout stable-$VERSION
+
+make
+make install
+
+cd ..
+rm -fr ansible
