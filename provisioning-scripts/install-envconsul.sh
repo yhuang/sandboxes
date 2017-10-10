625ab9cf4585843ab8187085a621b8866887c78e
Jimmy Y. Huang <jimmy.huang@duragility.com>
2017-10-10 06:47:25 -0700

    upgraded and added some HashiCorp software

--- provisioning-scripts/install-envconsul.sh
+++ provisioning-scripts/install-envconsul.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-APPLICATION=terraform
-VERSION=0.10.7
+APPLICATION=envconsul
+VERSION=0.7.2
 ARCHIVE=${APPLICATION}_${VERSION}_linux_amd64.zip
 
 wget https://releases.hashicorp.com/$APPLICATION/$VERSION/$ARCHIVE
