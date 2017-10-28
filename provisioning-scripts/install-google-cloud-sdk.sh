ecd7d23e9a2ff5f9b8e4285c1f43bd57e1c2f013
Jimmy Y. Huang <jimmy.huang@duragility.com>
2017-10-28 09:59:14 -0700

    switched from installing gcloud from yum to direct installation; upgraded terraform; made ks.cfg compatible with both CentOS 6 and 7; put the sleep step in packer under expect_disconnect true

---
+++ provisioning-scripts/install-google-cloud-sdk.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+APPLICATION=google-cloud-sdk
+VERSION=177.0.0
+ARCHIVE=${APPLICATION}-${VERSION}-linux-x86_64.tar.gz
+
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/$ARCHIVE
+
+tar xzvf $ARCHIVE -C /usr/local
+
+rm -f $ARCHIVE
+
+GOOGLE_CLOUD_SDK_DIR=/usr/local/google-cloud-sdk
+$GOOGLE_CLOUD_SDK_DIR/install.sh --disable-installation-options --quiet
+
+echo "pathmunge $GOOGLE_CLOUD_SDK_DIR/bin" > /etc/profile.d/google-cloud-sdk.sh
