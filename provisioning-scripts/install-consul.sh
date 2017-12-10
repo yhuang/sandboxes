76df7f03dccaf0ea92c70e3627d7394cfc110df1
Jimmy Y. Huang <jimmy.huang@duragility.com>
2017-12-09 21:23:39 -0800

    updated HashiCorp software

--- provisioning-scripts/install-consul.sh
+++ provisioning-scripts/install-consul.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 APPLICATION=consul
-VERSION=1.0.0
+VERSION=1.0.1
 ARCHIVE=${APPLICATION}_${VERSION}_linux_amd64.zip
 
 wget https://releases.hashicorp.com/$APPLICATION/$VERSION/$ARCHIVE
--- provisioning-scripts/install-packer.sh
+++ provisioning-scripts/install-packer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 APPLICATION=packer
-VERSION=1.1.2
+VERSION=1.1.3
 ARCHIVE=${APPLICATION}_${VERSION}_linux_amd64.zip
 
 wget https://releases.hashicorp.com/$APPLICATION/$VERSION/$ARCHIVE
