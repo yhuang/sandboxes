19b3c3195a67a7d61c4d8609f887f3df6ba91236
Jimmy Huang <Jimmy.Huang@creditkarma.com>
2017-12-19 22:29:55 -0800

    improved install-google-cloud-sdk.sh

--- provisioning-scripts/install-google-cloud-sdk.sh
+++ provisioning-scripts/install-google-cloud-sdk.sh
@@ -2,6 +2,7 @@
 
 APPLICATION=google-cloud-sdk
 VERSION=183.0.0
+ARCHIVE=${APPLICATION}-${VERSION}-linux-x86_64.tar.gz
 
 wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/$ARCHIVE
 
@@ -12,4 +13,10 @@ rm -f $ARCHIVE
 GOOGLE_CLOUD_SDK_DIR=/usr/local/google-cloud-sdk
 $GOOGLE_CLOUD_SDK_DIR/install.sh --disable-installation-options --quiet
 
-echo "pathmunge $GOOGLE_CLOUD_SDK_DIR/bin" > /etc/profile.d/google-cloud-sdk.sh
+GCLOUD_PATH=$GOOGLE_CLOUD_SDK_DIR/bin
+$GCLOUD_PATH/gcloud components update alpha --quiet
+
+echo "pathmunge $GCLOUD_PATH" > /etc/profile.d/google-cloud-sdk.sh
+
+SECURE_PATH="/sbin:/bin:/usr/sbin:/usr/bin:$GCLOUD_PATH"
+echo "Defaults    secure_path = $SECURE_PATH" > /etc/sudoers.d/gcloud
