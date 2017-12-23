d5688b7a6b4878de92453aa271f6362ffc7f6a8c
Jimmy Y. Huang <jimmy.huang@duragility.com>
2017-12-22 18:27:00 -0800

    installs golang 1.9.2

--- provisioning-scripts/install-go.sh
+++ provisioning-scripts/install-go.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=1.9
+VERSION=1.9.2
 
 cat > /etc/profile.d/goenv.sh << GOENV_SH
 export GOROOT=/opt/go
@@ -13,3 +13,8 @@ tar -C /opt -xzf go${VERSION}.linux-amd64.tar.gz
 rm go${VERSION}.linux-amd64.tar.gz
 
 chmod a+x /etc/profile.d/goenv.sh
+source /etc/profile.d/goenv.sh
+
+go get -u golang.org/x/tools/cmd/...
+go get -u github.com/golang/lint
+go get -u github.com/nsf/gocode
