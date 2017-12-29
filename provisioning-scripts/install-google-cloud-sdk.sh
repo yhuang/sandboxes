6158ca3d7a75012d4f6360ad785a8221a0d56de8
Jimmy Huang <Jimmy.Huang@creditkarma.com>
2017-12-28 19:43:57 -0800

    fixed bad conflict resolve

--- provisioning-scripts/install-google-cloud-sdk.sh
+++ provisioning-scripts/install-google-cloud-sdk.sh
@@ -6,7 +6,7 @@ ARCHIVE=${APPLICATION}-${VERSION}-linux-x86_64.tar.gz
 
 wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/$ARCHIVE
 
-tar xzvf $ARCHIVE -C /usr/local
+tar xzf $ARCHIVE -C /usr/local
 
 rm -f $ARCHIVE
 
@@ -16,8 +16,15 @@ $GOOGLE_CLOUD_SDK_DIR/install.sh --disable-installation-options --quiet
 GCLOUD_PATH=$GOOGLE_CLOUD_SDK_DIR/bin
 
 for i in alpha beta; do
-    $GCLOUD_PATH/gcloud components update $i
+    $GCLOUD_PATH/gcloud components update $i --quiet
+done
+
 echo "pathmunge $GCLOUD_PATH" > /etc/profile.d/google-cloud-sdk.sh
 
-SECURE_PATH="/sbin:/bin:/usr/sbin:/usr/bin:$GCLOUD_PATH"
-echo "Defaults    secure_path = $SECURE_PATH" > /etc/sudoers.d/gcloud
+OLD_PATH=$(grep 'Defaults\s*secure_path\s*=\s*\(.*\)' /etc/sudoers |
+awk -F'=' '{print $2}' |
+sed -e 's/^[ \t]*//')
+OLD_SECURE_PATH="Defaults    secure_path = $OLD_PATH"
+NEW_SECURE_PATH="Defaults    secure_path = $OLD_PATH:$GCLOUD_PATH"
+
+sed -i "s|$OLD_SECURE_PATH|$NEW_SECURE_PATH|" /etc/sudoers
