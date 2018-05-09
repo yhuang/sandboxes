ec5bcb43a21b51cc22dc0e3158d4c13c95abe2b0
Jimmy Y. Huang <jimmy.huang@duragility.com>
2018-05-09 06:46:28 -0700

    upgraded various software

--- provisioning-scripts/install-vault.sh
+++ provisioning-scripts/install-vault.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 APPLICATION=vault
-VERSION=0.10.0
+VERSION=0.10.1
 ARCHIVE=${APPLICATION}_${VERSION}_linux_amd64.zip
 
 wget https://releases.hashicorp.com/$APPLICATION/$VERSION/$ARCHIVE
