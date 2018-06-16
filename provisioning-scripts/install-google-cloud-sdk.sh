b2b1e7c8a6d926873e49d5bbea9e8bccc62db7b4
Jimmy Y. Huang <jimmy.huang@duragility.com>
2018-06-16 15:53:18 -0700

    upgraded software

--- provisioning-scripts/install-google-cloud-sdk.sh
+++ provisioning-scripts/install-google-cloud-sdk.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 APPLICATION=google-cloud-sdk
-VERSION=200.0.0
+VERSION=204.0.0
 ARCHIVE=${APPLICATION}-${VERSION}-linux-x86_64.tar.gz
 
 wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/$ARCHIVE
--- provisioning-scripts/install-packer.sh
+++ provisioning-scripts/install-packer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 APPLICATION=packer
-VERSION=1.2.3
+VERSION=1.2.4
 ARCHIVE=${APPLICATION}_${VERSION}_linux_amd64.zip
 
 wget https://releases.hashicorp.com/$APPLICATION/$VERSION/$ARCHIVE
