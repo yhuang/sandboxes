cb1c3e54ef67d333c6c559b016f8cc65576f6b72
Jimmy Y. Huang <jimmy.huang@duragility.com>
2018-07-23 00:14:17 -0700

    fixed parallel install

---
+++ provisioning-scripts/install-hcl2json.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+go get github.com/renier/hcl2json
--- provisioning-scripts/install-parallel.sh
+++ provisioning-scripts/install-parallel.sh
@@ -2,11 +2,26 @@
 
 VAGRANT_USER_HOME=/home/vagrant
 
-(wget -O - pi.dk/3 || curl pi.dk/3/) | bash
+# (wget -O - pi.dk/3 || curl pi.dk/3/) | bash
+# rm -fr .gnupg parallel-*
+
+export PATH="$PATH:/usr/local/bin"
+
+APPLICATION=parallel
+VERSION=20180722
+LATEST=${APPLICATION}-${VERSION}
+ARCHIVE=$LATEST.tar.gz
+
+wget http://git.savannah.gnu.org/cgit/parallel.git/snapshot/$ARCHIVE
+tar xzf	$ARCHIVE
+CURRENT_DIR=$(pwd)
+cd $LATEST || exit 2
+./configure && make && make install
 
 for user_dir in $VAGRANT_USER_HOME /root; do
     mkdir -p $user_dir/.parallel
     touch $user_dir/.parallel/will-cite
 done
 
-rm -fr .gnupg parallel-*
+cd $CURRENT_DIR
+rm -fr $ARCHIVE $LATEST
