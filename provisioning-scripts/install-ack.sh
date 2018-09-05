02074049127db3a3e9139ff3011033e70b3e4896
Jimmy Y. Huang <jimmy.huang@duragility.com>
2018-09-04 18:28:21 -0700

    updated all the packer templates; got devops box to build

--- provisioning-scripts/install-ack.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# https://beyondgrep.com/install
-yum --disablerepo="*" --enablerepo="epel" install -y ack
--- provisioning-scripts/install-cpanm.sh
+++ provisioning-scripts/install-cpanm.sh
@@ -3,3 +3,6 @@
 curl -L http://cpanmin.us | perl - --sudo App::cpanminus
 
 /usr/local/bin/cpanm App::pod2pdf
+
+# https://beyondgrep.com/install
+/usr/local/bin/cpanm App::Ack
