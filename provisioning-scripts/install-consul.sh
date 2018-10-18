d4aa4274d4b138a022cb38dce811a332b0eb2c57
Jimmy Y. Huang <jimmy.huang@duragility.com>
2018-10-18 02:44:28 -0700

    updated software versions

--- provisioning-scripts/install-consul.sh
+++ provisioning-scripts/install-consul.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 APPLICATION=consul
-VERSION=1.2.3
+VERSION=1.3.0
 ARCHIVE=${APPLICATION}_${VERSION}_linux_amd64.zip
 
 wget https://releases.hashicorp.com/$APPLICATION/$VERSION/$ARCHIVE
