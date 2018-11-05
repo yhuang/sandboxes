8f48e1fd1ae314fb41706121afecb57a6911fece
Jimmy Y. Huang <jimmy.huang@duragility.com>
2018-11-04 21:20:07 -0800

    upgraded packer

--- provisioning-scripts/install-packer.sh
+++ provisioning-scripts/install-packer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 APPLICATION=packer
-VERSION=1.3.1
+VERSION=1.3.2
 ARCHIVE=${APPLICATION}_${VERSION}_linux_amd64.zip
 
 wget https://releases.hashicorp.com/$APPLICATION/$VERSION/$ARCHIVE
