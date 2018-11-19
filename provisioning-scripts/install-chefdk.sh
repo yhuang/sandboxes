e777b589c0e1c214bc626f2b7d3eca9eec71e9fa
Jimmy Y. Huang <jimmy.huang@duragility.com>
2018-11-18 19:44:29 -0800

    updated software

--- provisioning-scripts/install-chefdk.sh
+++ provisioning-scripts/install-chefdk.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 APPLICATION=chefdk
-VERSION=3.3.23
+VERSION=3.5.13
 ARCHIVE=${APPLICATION}-${VERSION}-1.el7.x86_64.rpm
 
 rpm -Uvh https://packages.chef.io/files/stable/$APPLICATION/$VERSION/el/7/$ARCHIVE
---
+++ provisioning-scripts/install-exercism.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+APPLICATION=exercism
+VERSION=3.0.10
+ARCHIVE=${APPLICATION}-linux-64bit.tgz
+
+wget https://github.com/$APPLICATION/cli/releases/download/v$VERSION/$ARCHIVE
+
+tar xf $ARCHIVE
+mv $APPLICATION /usr/local/bin
+
+mkdir -p $HOME/.config/$APPLICATION
+mv ./shell/$APPLICATION* $HOME/.config/$APPLICATION
+
+cat > $HOME/.bash_profile << BASH_PROFILE
+if [ -f $HOME/.config/$APPLICATION/exercism_completion.bash ]; then
+  source $HOME/.config/$APPLICATION/exercism_completion.bash
+fi
+BASH_PROFILE
+
+cat >> $HOME/.zshrc << ZSHRC
+if [ -f $HOME/.config/$APPLICATION/exercism_completion.zsh ]; then
+  source $HOME/.config/$APPLICATION/exercism_completion.zsh
+fi
+ZSHRC
+
+rm -fr $ARCHIVE shell
