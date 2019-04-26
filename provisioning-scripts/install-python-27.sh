5d6af5a56d053a8ced7b24e055c7be37a42353af
Jimmy Y. Huang <jimmy.huang@duragility.com>
2019-04-26 07:08:47 -0700

    progress so far

--- provisioning-scripts/install-python-27.sh
+++ provisioning-scripts/install-python-27.sh
@@ -5,4 +5,9 @@ CENTOS_VERSION=$(rpm --query centos-release | cut -d"-" -f3)
 if [[ $CENTOS_VERSION -eq 6 ]]; then
     yum install -y centos-release-scl
     yum --enablerepo=centos-sclo-sclo install -y python27
+
+    LIB_PYTHON=/opt/rh/python27/root/usr/lib64
+
+    echo "$LIB_PYTHON" | tee -a /etc/ld.so.conf
+    ldconfig
 fi
--- provisioning-scripts/install-python-packages.sh
+++ provisioning-scripts/install-python-packages.sh
@@ -4,8 +4,12 @@ if hash python3 2>/dev/null; then
     python3 -m pip install --upgrade google-api-python-client
     python3 -m pip install --upgrade oauth2client
     python3 -m pip install requests
+    python3 -m pip install google-cloud-storage
+    python3 -m pip install google-auth
 fi
 
 pip install --upgrade google-api-python-client
 pip install --upgrade oauth2client
 pip install requests
+pip install google-cloud-storage
+pip install google-auth
