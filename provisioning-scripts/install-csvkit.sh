11fb72c976f5813991cdd96b4c0d65998538eefe
Jimmy Y. Huang <jimmy.huang@duragility.com>
2019-12-15 21:40:09 -0800

    fixed typo

--- provisioning-scripts/install-csvkit.sh
+++ provisioning-scripts/install-csvkit.sh
@@ -5,7 +5,7 @@ CENTOS_VERSION=$(rpm -E %{rhel})
 
 if [[ $CENTOS_VERSION -eq 6 ]]; then
     # https://centos.pkgs.org/6/openfusion-x86_64/csvkit-0.6.1-1.of.el6.noarch.rpm.html
-    wget http://repo.openfusion.net/centos6-x86_64/openfusion-release-0.7-1.o.el6.noarch.rpm
+    wget http://repo.openfusion.net/centos6-x86_64/openfusion-release-0.7-1.of.el6.noarch.rpm
     rpm -Uvh openfusion-release*rpm
     yum install -y csvkit
     rm -f openfusion-release*rpm
